a197350eb6a82808a61a5029461d22a3b1f34308
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-07-02 19:10:12 +0200

    Display image batch with actual class name

--- src/models/alexnet.py
+++ src/models/alexnet.py
@@ -119,6 +119,32 @@ class AlexNet(Sequential):
         )
 
 
+def get_mappings():
+    mappings = {}
+    f = open("F:\\caffe_ilsvrc12.tar\\caffe_ilsvrc12\\synset_words.txt")
+    for line in f:
+        splits = line.split(" ", 1)
+        folder_name = splits[0]
+        class_names = splits[1]
+        class_name = class_names.split(",", 1)[0]
+        class_name = class_name.rstrip()
+        mappings[folder_name] = class_name
+    f.close()
+    return mappings
+
+
+def show_batch(image_batch, label_batch):
+    mappings = get_mappings()
+    plt.figure(figsize=(10, 10))
+    for n in range(25):
+        plt.subplot(5, 5, n + 1)
+        plt.imshow(image_batch[n])
+        name = mappings[CLASS_NAMES[label_batch[n] == 1][0]]
+        plt.title(name)
+        plt.axis("off")
+    plt.show()
+
+
 def decode_image(img_path):
     img = tf.io.read_file(img_path)
     img = tf.image.decode_jpeg(img, channels=3)
