116fe78dbe73f303d8a6159fe2afa0ef1740a843
Amrit Purshotam <amrit.purshotam@gmail.com>
2020-09-08 19:29:47 +0200

    Make folder if not exists

--- src/callbacks/last_model_manager.py
+++ src/callbacks/last_model_manager.py
@@ -10,6 +10,9 @@ class LastModelManager(Callback):
         super().__init__()
 
     def on_epoch_end(self, epoch, logs=None):
+        if not os.path.exists(self.base_dir):
+            os.mkdir(self.base_dir)
+
         existing_models = os.listdir(self.base_dir)
         for existing_model in existing_models:
             if existing_model.startswith(f"last_model_{epoch:02d}"):
