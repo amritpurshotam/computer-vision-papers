288b374f577101876cca61b7a7e7dca41759a153
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-06-28 20:37:57 +0200

    Compute covariance with columns as variables
    This feels a more natural representation of the data

--- src/utilities/math.py
+++ src/utilities/math.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def cov(m: tf.Tensor) -> tf.Tensor:
+def cov(m: tf.Tensor, rowvar: bool = False) -> tf.Tensor:
     """Estimate a covariance matrix.
     Mimics the behaviour of `np.cov(m)` with default parameters i.e. `bias=False`
     and `rowvar=True`.
@@ -13,12 +13,23 @@ def cov(m: tf.Tensor) -> tf.Tensor:
         Each row of m represents a variable, and each column a single
         observation of all those variables.
 
+    rowvar: bool
+        If rowvar is True (default), then each row represents a variable, with
+        observations in the columns. Otherwise, the relationship is transposed:
+        each column represents a variable, while the rows contain observations.
+
     Returns
     -------
     tf.Tensor
         The covariance matrix of the variables.
     """
-    m = m - tf.reduce_mean(m, axis=1, keepdims=True)
-    normalization_factor = tf.cast(tf.shape(m)[1] - 1, tf.float32)
-    covariance = tf.matmul(m, tf.transpose(m)) / normalization_factor
-    return covariance
+    if rowvar:
+        m = m - tf.reduce_mean(m, axis=1, keepdims=True)
+        normalization_factor = tf.cast(tf.shape(m)[1] - 1, tf.float32)
+        covariance = tf.matmul(m, tf.transpose(m)) / normalization_factor
+        return covariance
+    else:
+        m = m - tf.reduce_mean(m, axis=0, keepdims=True)
+        normalization_factor = tf.cast(tf.shape(m)[0] - 1, tf.float32)
+        covariance = tf.matmul(tf.transpose(m), m) / normalization_factor
+        return covariance
