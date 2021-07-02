08965bb583d6cc78a329a0f24543095a75105021
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-07-02 14:17:56 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
 from setuptools import find_packages, setup
 
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
 setup(
     name="src",
-    packages=find_packages(),
+    packages=find_packages(include=["src"]),
     version="0.1.0",
     description="Computer Vision Models",
     author="Amrit Purshotam",
+    install_requires=install_requires,
+    extras_require={
+        "dev": [
+            "bandit==1.7.0",
+            "black==20.8b1",
+            "flake8==3.9.2",
+            "flake8-bandit==2.1.2",
+            "flake8-bugbear==21.4.3",
+            "flake8-builtins==1.5.3",
+            "flake8-comprehensions==3.5.0",
+            "isort==5.9.1",
+            "mypy==0.910",
+            "rope==0.19.0",
+        ],
+        "hooks": ["pre-commit==2.11.1"],
+        "notebook": [
+            "ipywidgets==7.6.3",
+            "jupyterlab==3.0.16",
+            "widgetsnbextension==3.5.1",
+        ],
+        "test": ["pytest==6.2.4"],
+    },
 )
