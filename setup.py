deee444af5c718e8a1393044518d5221e773f461
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-07-02 12:01:55 +0200

    Revert "Update requirements"
    This reverts commit 9c4c566fbd3cfa2101978e27800e5536bab86118.

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
 from setuptools import find_packages, setup
 
-with open("requirements.txt") as f:
-    install_requires = f.read().splitlines()
-
 setup(
     name="src",
-    packages=find_packages(include=["src"]),
+    packages=find_packages(),
     version="0.1.0",
     description="Computer Vision Models",
     author="Amrit Purshotam",
-    install_requires=install_requires,
-    extras_require={
-        "dev": [
-            "bandit==1.7.0",
-            "black==20.8b1",
-            "flake8==3.9.0",
-            "flake8-bandit==2.1.2",
-            "flake8-bugbear==21.4.3",
-            "flake8-builtins==1.5.3",
-            "flake8-comprehensions==3.4.0",
-            "isort==5.8.0",
-            "mypy==0.812",
-        ],
-        "hooks": ["pre-commit==2.11.1"],
-        "test": ["pytest==6.2.3"],
-    },
 )
