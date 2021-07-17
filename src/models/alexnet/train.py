e4ca0255da3dbd92b5a1030966f1b0c298cd2052
Amrit Purshotam <amrit.purshotam@gmail.com>
2021-07-17 15:34:07 +0200

    Reuse basedir to save model

--- src/models/alexnet/train.py
+++ src/models/alexnet/train.py
@@ -144,7 +144,7 @@ def train(dataset: str, group: str, name: str, apply_pca: bool, imagenet_pca: bo
         ],
     )
 
-    model.save(f".\\models\\alexnet\\{dataset}\\alexnet_final")
+    model.save(f"{base_dir}_model")
 
 
 if __name__ == "__main__":
